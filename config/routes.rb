2e771981251bb5ba0cee359d241cd80f221cb665
unknown <antoniodurvalc@gmail.com>
2021-12-12 11:52:37 -0300

    adicionando scaffold aluno

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :alunos
   resources :atividades
   resources :disciplinas
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_142400) do
+ActiveRecord::Schema.define(version: 2021_12_12_145206) do
+
+  create_table "alunos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "atividades", force: :cascade do |t|
     t.string "titulo"
