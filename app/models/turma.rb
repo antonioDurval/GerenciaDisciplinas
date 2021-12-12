20d341247b12a0559b3833af5b46febcfb99b210
unknown <antoniodurvalc@gmail.com>
2021-12-12 20:54:23 -0300

    validate turma

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,4 +1,8 @@
 class Turma < ApplicationRecord
   has_many :disciplinas, dependent: :destroy
   has_many :alunos, dependent: :destroy
+
+  validates :letra, length: { is: 1 }
+  validates :ano, presence: true, length: { maximum: 2}
+  validates :ano_letivo, presence: true, length: { minimum: 4}
 end
