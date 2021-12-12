82c577e4d8a1b590bd15e6f05e91cb204b548823
unknown <antoniodurvalc@gmail.com>
2021-12-12 10:52:27 -0300

    adicionando scaffold da disciplina

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_133017) do
+ActiveRecord::Schema.define(version: 2021_12_12_135042) do
+
+  create_table "disciplinas", force: :cascade do |t|
+    t.string "nome"
+    t.integer "ano_letivo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "turmas", force: :cascade do |t|
     t.string "letra"
