20579a88964c2d4d44f5a953fd903f4cc9befa72
unknown <antoniodurvalc@gmail.com>
2021-12-12 14:14:29 -0300

    adicionando link da avaliacao na pag inicial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_145206) do
+ActiveRecord::Schema.define(version: 2021_12_12_170940) do
 
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2021_12_12_145206) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["disciplina_id"], name: "index_atividades_on_disciplina_id"
+  end
+
+  create_table "avaliacaos", force: :cascade do |t|
+    t.float "pontos"
+    t.string "observacoes"
+    t.integer "aluno_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["aluno_id"], name: "index_avaliacaos_on_aluno_id"
   end
 
   create_table "disciplinas", force: :cascade do |t|
@@ -63,5 +72,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_145206) do
 
   add_foreign_key "alunos", "turmas"
   add_foreign_key "atividades", "disciplinas"
+  add_foreign_key "avaliacaos", "alunos"
   add_foreign_key "disciplinas", "turmas"
 end
