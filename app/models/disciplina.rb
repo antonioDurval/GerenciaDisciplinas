fc8b2aac2740d0a4e2520b723afc08c02ca35329
unknown <antoniodurvalc@gmail.com>
2021-12-12 20:57:13 -0300

    validate disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,4 +1,7 @@
 class Disciplina < ApplicationRecord
   belongs_to :turma
   has_many :atividades, dependent: :destroy
+
+  validates :nome, presence: true, length: {in: 4..30}
+  validates :ano_letivo, presence: true, length: { minimum: 4}
 end
