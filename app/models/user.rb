142a4d8305466f8fb4cc1db8e1b4539374a7a11b
unknown <antoniodurvalc@gmail.com>
2021-12-12 21:04:21 -0300

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nome, presence: true, length: {in: 5..20}
+  validates :password_confirmation, presence: true
 end
