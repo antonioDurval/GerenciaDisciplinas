32173f6e8e1b0b0d9d8e45bd36e64d7bbd2f2f01
unknown <antoniodurvalc@gmail.com>
2021-12-12 21:03:08 -0300

    validates atividade

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -1,4 +1,15 @@
 class Atividade < ApplicationRecord
   belongs_to :disciplina
   has_many :avaliacaos
+
+  validates :titulo, presence: true, length: {in: 5..20}
+  validates :bimestre, length: { is: 1 }
+  validates :data, presence: true
+  validate :verifica_data
+
+  def verifica_data
+    if data.present? && data < Time.zone.today
+      errors.add(:data, "nao pode ser no passado")
+    end
+  end
 end
